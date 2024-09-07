c7946060eee07d349c45aad8f997b09fb305e012
dyrex <Dimut280501@gmail.com>
2024-09-07 17:57:39 +0300

    Исправлен баг

--- src/main/java/ru/pizza/main_warehouse/domain/dto/response/to_restaurant/BuildingToRestaurantDTO.java
+++ src/main/java/ru/pizza/main_warehouse/domain/dto/response/to_restaurant/BuildingToRestaurantDTO.java
@@ -2,6 +2,8 @@ package ru.pizza.main_warehouse.domain.dto.response.to_restaurant;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -9,4 +11,16 @@ import lombok.*;
 public class BuildingToRestaurantDTO {
     private int id;
     private String title;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BuildingToRestaurantDTO that)) return false;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
